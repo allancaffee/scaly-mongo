872d076f1f7ca8e1e9df2be605019e0233cae680
Allan Caffee <allanc@aweber.com>
2012-01-01 19:58:24 -0500

    cursor: sort, et al return scalymongo cursors
    Previously calling sort, skip, limit, where, etc on scalymongo cursors
    would directly use the corresponding method on the wrapped cursor.  Since
    these methods return pymongo cursors they were returning plain old
    dictionaries when iterated.  The fix is to wrap the return value of any
    method that returns a cursor.

--- scalymongo/cursor.py
+++ scalymongo/cursor.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 
 
+def _make_cursor_wrapper_method(method_name):
+    """Make a wrapper method for `method_name`.
+
+    `method_name` should be the name of a method on
+    :class:`pymongo.cursor.Cursor` which returns another
+    :class:`pymongo.cursor.Cursor`.
+
+    The wrapper method will pass along any positional and/or keyword arguments
+    to the underlying method of the wrapped cursor and wrap the result in a
+    :class:`Cursor` so that it will still yield ScalyMongo documents.
+
+    """
+    def _wrapped_method(self, *args, **kwargs):
+        method = getattr(self._Cursor__wrapped_cursor, method_name)
+        returned = method(*args, **kwargs)
+        return Cursor(returned, self._Cursor__document_type)
+
+    return _wrapped_method
+
+
 class Cursor(object):
     """Wrapper for :class:`pymongo.cursor.Cursor` objects.
 
@@ -33,10 +53,16 @@ class Cursor(object):
     def __iter__(self):
         return self
 
-    def clone(self):
-        """Return a new wrapper :class:`Cursor`.
-        """
-        return Cursor(self.__wrapped_cursor.clone(), self.__document_type)
+    # Wrap all methods that return a pymongo.cursor.Cursor.
+    batch_size = _make_cursor_wrapper_method('batch_size')
+    clone = _make_cursor_wrapper_method('clone')
+    hint = _make_cursor_wrapper_method('hint')
+    limit = _make_cursor_wrapper_method('limit')
+    max_scan = _make_cursor_wrapper_method('max_scan')
+    rewind = _make_cursor_wrapper_method('rewind')
+    skip = _make_cursor_wrapper_method('skip')
+    sort = _make_cursor_wrapper_method('sort')
+    where = _make_cursor_wrapper_method('where')
 
     def __getattr__(self, attr):
         """All other methods and properties are those of the wrapped cursor.
